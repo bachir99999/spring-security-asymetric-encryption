58af8db362c9912432fc6f391dabcb4524ae0e62
Youssef B <bachir.benmalek@gmail.com>
2025-08-21 00:46:16 +0200

    git init

---
+++ src/test/java/com/bachir9/app/SpringSecurityAsymetricEncryptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bachir9.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAsymetricEncryptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
