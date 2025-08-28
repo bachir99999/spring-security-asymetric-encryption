027759a30f89c9b5c176e5d78b5c1b6f6d97e30c
Youssef B <bachir.benmalek@gmail.com>
2025-08-29 01:31:06 +0200

    auditor aware bean

--- src/main/java/com/bachir9/app/config/BeansConfig.java
+++ src/main/java/com/bachir9/app/config/BeansConfig.java
@@ -1,7 +1,9 @@
 package com.bachir9.app.config;
 
+import com.bachir9.app.role.ApplicationAuditorAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -17,6 +19,11 @@ public class BeansConfig {
     @Bean
     public AuthenticationManager authenticationManager(final AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuditorAware<String> auditorAware() {
+        return new ApplicationAuditorAware();
     }
 
 }
