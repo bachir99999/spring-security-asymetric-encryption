5e85fe8a0b84ae97ba2cdd92879ce05ccaaaa0e6
Youssef B <bachir.benmalek@gmail.com>
2025-08-31 07:27:40 +0200

    open api config

---
+++ src/main/java/com/bachir9/app/config/OpenAPIConfig.java
@@ -0,0 +1,41 @@
+package com.bachir9.app.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(name = "Spring security jwt asymmetric encryption", url = "https://github.com/bachir99999/spring-security-asymetric-encryption"),
+                description = "OpenAPI doc for spring security",
+                version = "1.0",
+                title = "OpenAPI spec"
+        ),
+        servers = {
+              @Server(
+                        description = "Local ENV",
+                        url = "http://localhost:8080"
+                )
+        },
+        security = {
+                @SecurityRequirement(
+                        name = "bearerAuth"
+                )
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT auth description",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenAPIConfig {
+
+}
